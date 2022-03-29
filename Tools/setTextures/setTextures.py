f85c440d0e8cddb8edbdffc9c6f76396cc12ec74
dprojects <dprojects@users.noreply.github.com>
2022-03-29 21:17:39 +0200

    setTextures update

--- Tools/setTextures/setTextures.py
+++ Tools/setTextures/setTextures.py
@@ -2,7 +2,7 @@
 
 # FreeCAD macro for woodworking to apply and store textures
 # Author: Darek L (aka dprojects)
-# Version: 4.0
+# Version: 5.0
 # Latest version: https://github.com/dprojects/setTextures
 
 import FreeCAD, FreeCADGui
@@ -20,8 +20,6 @@ import os
 
 def showQtMain():
 
-	global gExecute
-
 	# ############################################################################
 	# Qt Main Class
 	# ############################################################################
@@ -37,7 +35,7 @@ def showQtMain():
 			
 			# window
 			self.result = userCancelled
-			self.setGeometry(400, 250, 800, 250)
+			self.setGeometry(400, 250, 800, 350)
 			self.setWindowTitle("setTextures")
 			self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
 
@@ -46,32 +44,73 @@ def showQtMain():
 			# ############################################################################
 
 			# label
-			self.pathL = QtGui.QLabel("Texture URL path:", self)
-			self.pathL.move(10, 40)
+			info = ""
+			info += "If you want to load all stored textures, please go to Step 3, without selection, and click load button."
+			self.step0 = QtGui.QLabel(info, self)
+			self.step0.move(10, 10)
+
+			# label
+			info = ""
+			info += "Step 1. Add texture URL path:"
+			self.step1 = QtGui.QLabel(info, self)
+			self.step1.move(10, 40)
 
 			# text input
 			self.pathI = QtGui.QLineEdit(self)
 			self.pathI.setText(str(""))
 			self.pathI.setFixedWidth(750)
-			self.pathI.move(10, 63)
+			self.pathI.move(10, 60)
 
 			# label
-			self.pathSL = QtGui.QLabel("Store given URL to all selected objects at property named Texture (if no URL this property will be empty).", self)
-			self.pathSL.move(100, 103)
+			info = ""
+			info += "Step 2. Store given URL at property named Texture"
+			info += ", if no URL this property will be set to empty string:"
+			self.step2 = QtGui.QLabel(info, self)
+			self.step2.move(10, 100)
 
 			# button
 			self.pathS = QtGui.QPushButton("store", self)
 			self.pathS.clicked.connect(self.setTextureProperty)
-			self.pathS.move(10, 100)
+			self.pathS.move(10, 120)
+			self.pathS.resize(80, 40)
 
 			# label
-			self.pathGL = QtGui.QLabel("Download and apply textures from stored URLs (for all objects or selected only).", self)
-			self.pathGL.move(100, 133)
+			info = ""
+			info += "- Selected objects - if objects selected e.g. good to change texture for single object only"
+			self.step2a = QtGui.QLabel(info, self)
+			self.step2a.move(100, 123)
+
+			# label
+			info = ""
+			info += "- All objects - if no objects selected e.g. good for merged projects and many objects"
+			self.step2b = QtGui.QLabel(info, self)
+			self.step2b.move(100, 143)
+
+
+			# label
+			info = ""
+			info += "Step 3. Download and apply textures from stored URLs:"
+			self.step3 = QtGui.QLabel(info, self)
+			self.step3.move(10, 200)
 
 			# button
 			self.pathG = QtGui.QPushButton("load", self)
 			self.pathG.clicked.connect(self.loadStoredTextures)
-			self.pathG.move(10, 130)
+			self.pathG.move(10, 220)
+			self.pathG.resize(80, 40)
+
+			# label
+			info = ""
+			info += "- Selected objects - if objects selected e.g. good to refresh changed textures"
+			self.step3a = QtGui.QLabel(info, self)
+			self.step3a.move(100, 223)
+
+			# label
+			info = ""
+			info += "- All objects - if no objects selected e.g. good to load all stored textures"
+			self.step3b = QtGui.QLabel(info, self)
+			self.step3b.move(100, 243)
+
 
 			# label
 			space = ""
@@ -79,7 +118,7 @@ def showQtMain():
 			space += "                                                                                        "
 			space += "                                                                                        "
 			self.Status = QtGui.QLabel(space, self)
-			self.Status.move(10, 200)
+			self.Status.move(10, 320)
 
 			# ############################################################################
 			# show
@@ -105,18 +144,29 @@ def showQtMain():
 
 		def setTextureProperty(self):
 
+			# check if for selected only or all
 			selected = FreeCADGui.Selection.getSelection()
 			selectedLen = len(selected)
 
-			if selectedLen == 0:
-				iText = ""
-				iText += "No selected objects found. \n" 
-				iText += "Please select objects to add property. \n"
-				self.showStatus(iText)
+			if selectedLen > 0:
+				searchObjects = selected
+			else:
+				searchObjects = FreeCAD.activeDocument().Objects
+
 		
 			textureURL = self.pathI.text()
 
-			for obj in selected:
+			for obj in searchObjects:
+
+				# skip everything except furniture parts if for all
+				if (
+					selectedLen == 0 and
+					not obj.isDerivedFrom("Part::Box") and 
+					not obj.isDerivedFrom("PartDesign::Pad")
+				):
+					continue
+
+				# set property
 				try:
 					if not hasattr(obj, "Texture"):
 						obj.addProperty("App::PropertyString", "Texture", "Base", "")
@@ -138,7 +188,7 @@ def showQtMain():
 			if not os.path.exists(tmpDir):
 				os.makedirs(tmpDir)
 
-			# check if load for selected only or all
+			# check if for selected only or all
 			selected = FreeCADGui.Selection.getSelection()
 			selectedLen = len(selected)
 
@@ -154,30 +204,27 @@ def showQtMain():
 	
 				# support for texture URL stored at objects description	
 				try:
-					if textureURL == "":
-						ref = str(obj.Label2)
-						if ref != "" and ref.startswith("http"):
-							textureURL = ref
+					ref = str(obj.Label2)
+					if ref != "" and ref.startswith("http"):
+						textureURL = ref
 				except:
-					textureURL = ""
+					skip = 1
 	
 				# support for texture URL stored at objects Texture property
 				try:
-					if textureURL == "":
-						ref = str(obj.Texture)
-						if ref != "" and ref.startswith("http"):
-							textureURL = ref
+					ref = str(obj.Texture)
+					if ref != "" and ref.startswith("http"):
+						textureURL = ref
 				except:
-					textureURL = ""
+					skip = 1
 	
 				# support for texture URL stored at Material Card TexturePath
 				try:
-					if textureURL == "":
-						ref = str(obj.Material.Material["TexturePath"])
-						if ref != "" and ref.startswith("http"):
-							textureURL = ref
+					ref = str(obj.Material.Material["TexturePath"])
+					if ref != "" and ref.startswith("http"):
+						textureURL = ref
 				except:
-					textureURL = ""
+					skip = 1
 		
 				# if no texture found for object skip it
 				if str(textureURL) == "":
