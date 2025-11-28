51c039ff569af76b1bc9eee282d9e9e236735928
dprojects <dprojects@users.noreply.github.com>
2025-11-28 15:26:08 +0100

    fix TOC creation for docs

--- Docs/makeTOC.py
+++ Docs/makeTOC.py
@@ -13,83 +13,64 @@ file =  open(sys.argv[1], 'r')
 data = file.readlines()
 
 for line in data:
-	if line.startswith("#") == True:
+		
+		# ########################################
+		# common rules
+		# ########################################
+		
+		if line.startswith("#") == True:
+			line = line.replace("\n","")
+			line = line.replace(",","")
+			line = line.replace(".","")
+			
+			# debug
+			#toc.append(line)
+			#continue
+			
+			head = line
+			link = line
+			
+			link = line.lower()
+			link = link.replace(" ","-")
+			link = link.replace("#-","# ")
+		
+		# ########################################
+		# individual rules
+		# ########################################
 		
 		if line.startswith("# ") == True:
-			
-			link = line.replace("\n","")
-			link = link.lower()
 			link = link.replace("# ","")
-			link = link.replace(" ","-")
-			link = link.replace(",","")
-			
-			head = line.replace("\n","")
 			head = head.replace("# ","")
-			
 			entry = "* [" + head + "](#" + link + ")"
-		
 			toc.append(entry)
 			
 		if line.startswith("## ") == True:
-			
-			link = line.replace("\n","")
-			link = link.lower()
 			link = link.replace("## ","")
-			link = link.replace(" ","-")
-			link = link.replace(",","")
-			
-			head = line.replace("\n","")
 			head = head.replace("## ","")
-			
 			entry = "\t" + "* [" + head + "](#" + link + ")"
-		
 			toc.append(entry)
 			
 		if line.startswith("### ") == True:
-			
-			link = line.replace("\n","")
-			link = link.lower()
 			link = link.replace("### ","")
-			link = link.replace(" ","-")
-			link = link.replace(",","")
-			
-			head = line.replace("\n","")
 			head = head.replace("### ","")
-			
 			entry = "\t\t" + "* [" + head + "](#" + link + ")"
-		
 			toc.append(entry)
 			
 		if line.startswith("#### ") == True:
-			
-			link = line.replace("\n","")
-			link = link.lower()
 			link = link.replace("#### ","")
-			link = link.replace(" ","-")
-			link = link.replace(",","")
-			
-			head = line.replace("\n","")
 			head = head.replace("#### ","")
-			
 			entry = "\t\t\t" + "* [" + head + "](#" + link + ")"
-		
 			toc.append(entry)
 			
 		if line.startswith("##### ") == True:
-			
-			link = line.replace("\n","")
-			link = link.lower()
 			link = link.replace("##### ","")
-			link = link.replace(" ","-")
-			link = link.replace(",","")
-			
-			head = line.replace("\n","")
 			head = head.replace("##### ","")
-			
 			entry = "\t\t\t\t" + "* [" + head + "](#" + link + ")"
-		
 			toc.append(entry)
 
-
+# ########################################
+# final print
+# ########################################
+		
 for t in toc:
 	print(t)
